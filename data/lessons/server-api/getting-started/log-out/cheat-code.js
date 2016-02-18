f8db9a92934d0c6a6d92d3cf722ab125a8775190
Sébastien Heymann <seb@linkurio.us>
2016-02-18 18:43:42 +0100

    Update to qwest 4.1.1

--- data/lessons/server-api/getting-started/log-out/cheat-code.js
+++ data/lessons/server-api/getting-started/log-out/cheat-code.js
@@ -1,7 +1,6 @@
 var url = 'http://crunchbase.linkurio.us/api/auth/logout';
 
 qwest.get(url, null, {
-  cache: true,
-  responseType: 'text' // bug https://github.com/pyrsmk/qwest/issues/102
+  cache: true
 })
 .then(test).catch(error);
