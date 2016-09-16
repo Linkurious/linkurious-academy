da5c9275070a974029b754f7fa6996b1eb562af5
Sébastien Heymann <seb@linkurio.us>
2016-09-16 08:34:27 +0200

    Update cheat-code.js

--- data/lessons/server-api/open-ui/graph-query/cheat-code.js
+++ data/lessons/server-api/open-ui/graph-query/cheat-code.js
@@ -3,6 +3,7 @@ var BASE_URL = 'http://crunchbase.linkurio.us/';
 var query = 'MATCH (n)-[r]-(m) RETURN n,r,m LIMIT 5';
 var populate = 'pattern';
 var dialect = 'cypher' // 'cypher' for Neo4j or 'gremlin' for TitanDB
+var layout = true; // run the ForceLink layout on the server
 
 qwest.get(BASE_URL + 'api/dataSources', null, {
   cache: true
@@ -12,6 +13,7 @@ qwest.get(BASE_URL + 'api/dataSources', null, {
   if (source && source.connected && source.state == 'ready') {
     window.open(BASE_URL + 'workspace/new' +
       '?source=' + source.key +
+      '&do_layout=' + layout +
       '&populate=' + populate +
       '&pattern_query=' + encodeURIComponent(query) +
       '&pattern_dialect=' + dialect
